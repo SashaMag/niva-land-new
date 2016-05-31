4689d47e813851d408ea9080da891a7bf88c9e85
tyaglovOV <tyaglov_ov@outlook.com>
2016-05-31 18:14:25 +0300

    add description, images, anchor

--- js/script.js
+++ js/script.js
@@ -5,11 +5,19 @@
 
   var popupWindow = document.querySelector('.b-popup');
   var closeCross = document.querySelector('.close-cross span');
+  var closeCross2 = document.querySelector('.close-cross-2 span');
   var buttons = document.querySelectorAll('.popup-form-button');
   var popupHeaderHeadline = document.querySelector('.popup-header h3');
   var popupHeaderText = document.querySelector('.popup-header .description');
   var popupForm = document.querySelector('.popup-form');
   var popupInfo = document.querySelector('.popup-info');
+
+  function hideAllVisibles() {
+    var visibleClasses = document.querySelectorAll('.visible');
+    for (var i = 0; i < visibleClasses.length; i++) {
+      visibleClasses[i].classList.remove('visible');
+    }
+  }
 
   for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', showPopupWindow);
@@ -17,27 +25,22 @@
 
   function showPopupWindow() {
 
-    if (this.dataset.popupType == 'form') {
+    hideAllVisibles();
+
+    if (this.dataset.popupType === 'form') {
 
       popupForm.classList.add('visible');
+      popupHeaderHeadline.innerHTML = this.dataset.header;
+      popupHeaderText.innerHTML = this.dataset.description;
 
-      switch (this.dataset.popupHeader) {
+    } else if (this.dataset.popupType === 'info') {
+      console.log('.niva-' + this.dataset.cartype);
 
-        case ('in-data') :
-          popupHeaderHeadline.innerHTML = this.dataset.header;
-          popupHeaderText.innerHTML = this.dataset.description;
-          break;
-
-        default :
-          break;
-      }
-
-      popupInfo.classList.remove('visible');
-
-    } else if (this.dataset.popupType == 'info') {
-
-      popupForm.classList.remove('visible');
       popupInfo.classList.add('visible');
+      var visibleBlock = document.querySelector('.' + this.dataset.infoType);
+      visibleBlock.classList.add('visible');
+      visibleBlock.querySelector('.' + this.dataset.cartype).classList.add('visible');
+
     }
 
 
@@ -46,13 +49,27 @@
 
 
   function closePopupWindow(event) {
-    if(event.target == popupWindow || event.target == closeCross) {
+    if(event.target == popupWindow || event.target == closeCross || event.target == closeCross2) {
       popupWindow.classList.remove('active');
     }
   }
 
   popupWindow.addEventListener('click', closePopupWindow);
   closeCross.addEventListener('click', closePopupWindow);
+  closeCross2.addEventListener('click', closePopupWindow);
+
+  //плавный якорь
+  $(document).ready(function() {
+
+    $('a[href^="#"]').click(function(){
+      var el = $(this).attr('href');
+      $('body').animate({
+        scrollTop: $(el).offset().top}, 500);
+      return false;
+    });
+
+  });
+
 })();
 
 
